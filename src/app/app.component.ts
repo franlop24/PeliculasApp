ae5a91e4b6ecbd1df75dc63e6172b7179edbca71
Francisco Lopez <franlopbri@gmail.com>
2020-10-16 14:31:42 -0500

    Componente Slideshow Estructura y argumentos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { PeliculasService } from './services/peliculas.service';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +7,5 @@ import { PeliculasService } from './services/peliculas.service';
 })
 export class AppComponent {
   title = 'peliculasApp';
-  constructor(private peliculasService: PeliculasService){
-    this.peliculasService.getCartelera()
-        .subscribe(data => {
-          console.log(data)
-        })
-  }
+  
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/interfaces/cartelera-response';
+import { PeliculasService } from 'src/app/services/peliculas.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public movies: Movie[] = [];
+
+  constructor(private pelisService: PeliculasService) { 
+    
+  }
 
   ngOnInit(): void {
+    this.pelisService.getCartelera()
+      .subscribe(peliculas => {
+        this.movies = peliculas.results
+      })
   }
 
 }
