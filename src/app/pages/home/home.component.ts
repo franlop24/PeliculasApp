e7157e8745f0b8ba532cd195ce9365194d6824a7
Francisco Lopez <franlopbri@gmail.com>
2020-10-21 11:39:50 -0500

    implementacion infinite scroll parte 1

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/cartelera-response';
 import { PeliculasService } from 'src/app/services/peliculas.service';
 
@@ -10,6 +10,20 @@ import { PeliculasService } from 'src/app/services/peliculas.service';
 export class HomeComponent implements OnInit {
 
   public movies: Movie[] = [];
+  public moviesSlideshow: Movie[] = [];
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(){
+
+    const pos = (document.documentElement.scrollTop || document.body.scrollTop) + 1000;
+    const max = (document.documentElement.scrollHeight || document.body.scrollHeight);
+
+    if(pos > max){
+      this.pelisService.getCartelera().subscribe( resp => {
+        this.movies.push(...resp.results);
+      })
+    }
+  }
 
   constructor(private pelisService: PeliculasService) { 
     
@@ -18,7 +32,8 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.pelisService.getCartelera()
       .subscribe(peliculas => {
-        this.movies = peliculas.results
+        this.movies = peliculas.results;
+        this.moviesSlideshow = peliculas.results;
       })
   }
 
--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -2,15 +2,33 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CarteleraResponse } from '../interfaces/cartelera-response';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeliculasService {
 
+  private baseUrl: string = 'https://api.themoviedb.org/3';
+  private carteleraPage = 1;
+
   constructor(private http: HttpClient) { }
 
+  get params(){
+    return {
+      api_key: '6fa4238f26b1e74007e989874a385ba1',
+      language: 'es-ES',
+      page: this.carteleraPage.toString()
+    }
+  }
+
   getCartelera():Observable<CarteleraResponse>{
-    return this.http.get<CarteleraResponse>(' https://api.themoviedb.org/3/movie/popular?api_key=6fa4238f26b1e74007e989874a385ba1&language=es-ES&page=1')
+    return this.http.get<CarteleraResponse>(`${ this.baseUrl }/movie/now_playing`,{
+      params: this.params
+    }).pipe(
+      tap( () => {
+        this.carteleraPage += 1;
+      })
+    );
   }
 }
