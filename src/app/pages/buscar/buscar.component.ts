895a6001130b53b5a3034bd1fb38ce6e8291c3e1
Francisco Lopez <franlopbri@gmail.com>
2020-10-21 13:43:44 -0500

    se implementa la busqueda en el servicio

--- src/app/pages/buscar/buscar.component.ts
+++ src/app/pages/buscar/buscar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PeliculasService } from 'src/app/services/peliculas.service';
 
 @Component({
   selector: 'app-buscar',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private peliculasService: PeliculasService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe( params => {
+      console.log(params);
+      //Llamar el servicio
+      this.peliculasService.buscarPeliculas(params.texto)
+          .subscribe( movies => {
+            console.log(movies);
+          } )
+    } )
   }
 
 }
