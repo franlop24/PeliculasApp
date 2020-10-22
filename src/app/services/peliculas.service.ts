14ac97c8e5231b0935a9dedbc2c66f2b36c9b0e3
Francisco Lopez <franlopbri@gmail.com>
2020-10-22 15:07:23 -0500

    se implementa el metodo de consulta detalle de la API

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { CarteleraResponse, Movie } from '../interfaces/cartelera-response';
 import { map, tap } from 'rxjs/operators';
+import { MovieResponse } from '../interfaces/movie-response';
 
 @Injectable({
   providedIn: 'root'
@@ -49,6 +50,12 @@ export class PeliculasService {
     }).pipe(
       map(resp => resp.results)
     )
+  }
+
+  getPeliculaDetalle(id: string){
+    return this.http.get<MovieResponse>(`${ this.baseUrl }/movie/${ id }`,{
+      params: this.params 
+    })
   }
 
   resetCarteleraPage(){
