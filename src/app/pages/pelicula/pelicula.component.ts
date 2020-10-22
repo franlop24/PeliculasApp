2c1f3bcdca44099b833bbbf1a341c23877525bee
Francisco Lopez <franlopbri@gmail.com>
2020-10-22 15:34:24 -0500

    implementacion de diseno de la pantalla detalle

--- src/app/pages/pelicula/pelicula.component.ts
+++ src/app/pages/pelicula/pelicula.component.ts
@@ -1,5 +1,7 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MovieResponse } from 'src/app/interfaces/movie-response';
 import { PeliculasService } from 'src/app/services/peliculas.service';
 
 @Component({
@@ -9,16 +11,22 @@ import { PeliculasService } from 'src/app/services/peliculas.service';
 })
 export class PeliculaComponent implements OnInit {
 
+  public movie: MovieResponse
+
   constructor(private activatedRoute: ActivatedRoute,
-              private peliculasService: PeliculasService) { }
+              private peliculasService: PeliculasService,
+              private location: Location) { }
 
   ngOnInit(): void {
     const { id } = this.activatedRoute.snapshot.params;
 
     this.peliculasService.getPeliculaDetalle(id)
         .subscribe(movie => {
-          console.log(movie);
+          this.movie = movie;
         });
   }
 
+  onRegresar(){
+    this.location.back();
+  }
 }
